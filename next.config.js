ee918f01cae2cfc899e5e3561c18c7c8e26cf996
Andrew <66430340+aandrew-me@users.noreply.github.com>
2023-01-10 17:57:50 +0600

    Trying to fix build errors

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    unoptimized: true,
-  },
+	reactStrictMode: true,
+	swcMinify: true,
+	images: {
+		unoptimized: true,
+	},
+	env: { NODE_OPTIONS: "--max_old_space_size=4096" },
 };
 
 module.exports = nextConfig;
